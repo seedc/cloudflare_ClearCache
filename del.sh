2400446a101d672ffee9ef956bdff06492acd255
seedc <seedccy@gmail.com>
2024-07-25 10:53:21 +0800

    Update del.sh

--- del.sh
+++ del.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # 清理实例
-curl -X POST -H "Content-Type: application/json" -d "{\"domain\":\"${1}\"}" http://cf-cache.default.svc.cluster.local:8000/api/v1/domain
+# curl -X POST -H "Content-Type: application/json" -d "{\"domain\":\"${1}\"}" http://cf-cache.default.svc.cluster.local:8000/api/v1/domain
+curl -X POST -H "Content-Type: application/json" -d "{\"domain\":\"${1}\"}" http://127.0.0.1:8000/api/v1/domain
